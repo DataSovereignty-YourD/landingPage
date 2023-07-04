5a9de2b90b3bb22cd57376d9129458b0b3b366d0
김명석 <75840431+cacaocoffee@users.noreply.github.com>
2023-07-04 22:48:08 +0800

    footer update
    footer update

--- src/component/footer.tsx
+++ src/component/footer.tsx
@@ -1,15 +1,31 @@
 import React from "react";
 import twitter from "../assets/img/twitter.png";
 import linkedin from "../assets/img/linkedin.png";
+import logo from "../assets/img/YourD-Logo-high.png";
 const Footer = () => {
   return (
     <footer className="bg-gray-200 py-4 px-10 flex flex-row justify-start">
-      <div className="container mx-auto text-center">
-        <p className="text-gray-600 text-left text-xl">ⓒ YOURD</p>
+      <div className="container mx-auto ali text-center">
+        <p className="text-gray-600 text-left">
+          © 2023 yourD - Altme All rights reserved
+        </p>
       </div>
       <div id="icon" className="flex flex-row justify-right mr-20">
-        <img src={twitter} className="h-10 w-auto mr-2"></img>
-        <img src={linkedin} className="h-10 w-auto"></img>
+        <a
+          href="https://twitter.com/0xCatbox"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={twitter} className="h-6 w-auto mr-2"></img>
+        </a>
+
+        <a
+          href="https://www.linkedin.com/company/yourd/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={linkedin} className="h-6 w-auto"></img>
+        </a>
       </div>
     </footer>
   );
