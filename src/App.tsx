b0ef027b04cd9f0daac4ae67ac797cea8342945d
김명석 <75840431+cacaocoffee@users.noreply.github.com>
2023-07-05 18:37:48 +0800

    Feature/noVideoPage
    비디오가 없는 설명 페이지 작성

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,18 @@ import FAQPage from "./pages/FAQPage";
 import MainPage from "./pages/mainPage";
 import TopNavi from "./component/topNavi";
 import YourDIntroPage from "./pages/yourdIntroPage";
-import CarouselExample from "./pages/test";
 import Footer from "./component/footer";
 import FirstPage from "./pages/FirstPage";
+import NoVideoPage from "./pages/Novideopage";
 
 function App() {
   return (
     <div>
-      <TopNavi/>
-      <FirstPage/>
+      <TopNavi />
+      <FirstPage />
       <YourDIntroPage />
       {/* <DetailPage /> */}
+      <NoVideoPage />
       <FAQPage />
       <Footer />
     </div>
