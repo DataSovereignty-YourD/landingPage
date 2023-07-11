97e567a3aa98899b3ee6a4f4d94e9a43c088590f
김명석 <75840431+cacaocoffee@users.noreply.github.com>
2023-07-11 14:01:06 +0900

    feature Animation for FAQs
    Animation put in FAQs page

--- src/pages/FAQPage.tsx
+++ src/pages/FAQPage.tsx
@@ -33,20 +33,27 @@ const FAQPage: React.FC = () => {
       <div className=" text-4xl  font-[font-sf-pro-text-bold] font-bold sm:text-center text-custom-blue mb-4">
         FAQs
       </div>
+
       {FAQs.map((faq, index) => (
         <div
-          key={index}
-          className=" border-b-2 p-2 mb-4 w-full md:w-3/4 lg:w-2/3  mx-auto transition-all duration-200 ease-in h-fit"
+          className=" border-b-2 p-2 mb-4 w-full my-2 sm:my-4 md:my-6 md:w-3/4 lg:w-2/3  mx-auto h-fit"
+          onClick={() => setActiveIndex(index === activeIndex ? null : index)}
         >
-          <p
-            className="font-bold font-[font-sf-pro-text-bold] text-xl sm:text-2xl lg:text-3xl "
-            onClick={() => setActiveIndex(index === activeIndex ? null : index)}
-          >
+          <p className="font-bold font-[font-sf-pro-text-bold] text-xl sm:text-2xl lg:text-3xl justify-between flex flex-row cursor-pointer">
             {faq.question}
+            <svg className="fill-current text-custom-blue h-6 w-6 transform transition-transform duration-500">
+              <path d="M13.962,8.885l-3.736,3.739c-0.086,0.086-0.201,0.13-0.314,0.13S9.686,12.71,9.6,12.624l-3.562-3.56C5.863,8.892,5.863,8.611,6.036,8.438c0.175-0.173,0.454-0.173,0.626,0l3.25,3.247l3.426-3.424c0.173-0.172,0.451-0.172,0.624,0C14.137,8.434,14.137,8.712,13.962,8.885 M18.406,10c0,4.644-3.763,8.406-8.406,8.406S1.594,14.644,1.594,10S5.356,1.594,10,1.594S18.406,5.356,18.406,10 M17.521,10c0-4.148-3.373-7.521-7.521-7.521c-4.148,0-7.521,3.374-7.521,7.521c0,4.147,3.374,7.521,7.521,7.521C14.148,17.521,17.521,14.147,17.521,10"></path>
+            </svg>
           </p>
-          {index === activeIndex && (
-            <p className="mt-4 text-sm sm:text-lg lg:text-xl font-[font-sf-pro-text] leading-normal">{faq.answer}</p>
-          )}
+          <div
+            className={`border-l-2 border-custom-blue px-6 pt-0 overflow-hidden transition-[max-height] duration-500 ease-in ${
+              index === activeIndex ? "max-h-96" : "max-h-0"
+            }`}
+          >
+            <p className="text-sm sm:text-lg lg:text-xl font-[font-sf-pro-text] leading-normal">
+              {faq.answer}
+            </p>
+          </div>
         </div>
       ))}
     </div>
