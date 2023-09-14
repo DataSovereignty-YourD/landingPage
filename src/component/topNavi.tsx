803eff71047f438bf55106e138f2c45b7a75f08a
4uphwang <4uphwang@gmail.com>
2023-09-14 20:58:20 +0900

    viewHook animation

--- src/component/topNavi.tsx
+++ src/component/topNavi.tsx
@@ -8,27 +8,13 @@ import linkedin from "../assets/img/linkedin.png";
 import instagram from "../assets/img/instagram.png";
 import discord from "../assets/img/discord.png";
 export default function TopNavi() {
+
   return (
     <div className="h-12 bg-black sm:h-16 w-full text-sm sm:text-xl bg-transparent fixed pr-4 lg:px-32 md:px-20 sm:px-20 sm:mr-20 transition-all duration-100 flex justify-between items-center backdrop-blur-md z-30">
       <HashLink smooth to="/#">
         <img src={logo} className="h-12 sm:h-16 object-cover " alt="Logo" />
       </HashLink>
-      {/* <HashLink
-          smooth
-          to="/#Developer"
-          className=" text-2l w-fit mx-auto font-sf-pro-text font-bold text-center align-center"
-        >
-          Developers
-        </HashLink> */}
-
-      {/* <HashLink
-          smooth
-          to="/#Product"
-          className=" text-2l w-fit mx-auto font-sf-pro-text font-bold text-center align-center"
-        >
-          Product
-        </HashLink> */}
-      <div className="flex justify-center items-center gap-2">
+      {/* <div className="flex justify-center items-center gap-2">
         <a
           href="https://twitter.com/0xCatbox"
           target="_blank"
@@ -60,14 +46,13 @@ export default function TopNavi() {
         >
           <img src={linkedin} className="h-8 w-auto object-contain"></img>
         </a>
-
         <a
           className=" w-fit mx-auto font-sf-pro-text font-bold text-center align-center text-white rounded-3xl border border-black py-2 px-3 hover:bg-[#007aff] transition-all duration-300 hover:text-[#FFD400]"
           href="mailto:0xcatbox@gmail.com"
         >
           Contact
         </a>
-      </div>
+      </div> */}
     </div>
   );
 }
