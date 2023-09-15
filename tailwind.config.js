ec78756e3ad85db74cdd4905bf13a091870b7894
yeomjaeseung <struckpang@gmail.com>
2023-09-16 00:58:11 +0900

    apple

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   mode: "jit",
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   content: [],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {
       backgroundImage: {
