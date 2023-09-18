20e8ef10eb2714ce0cb9043ed10c0cbff45cb26f
parkerynch <110054754+parkerynch@users.noreply.github.com>
2023-09-19 00:20:09 +0900

    banana comeback

--- src/Appppp.jsx
+++ src/Appppp.jsx
@@ -3,14 +3,14 @@ import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
 import * as THREE from "three";
 
-function App() {
+export default function AnimationReal() {
   return (
-    <div className="fullscreen-container">
+    <div className="w-full h-full fixed z-[1]">
       <Canvas
         className="w-full h-full"
         camera={{ position: [0, 0, 5], fov: 30 }}
       >
-        <color attach="background" args={["#000000"]} />
+        <color attach="background" args={["#ffffff"]} />
         <ScrollControls pages={12}>
           <Experience />
         </ScrollControls>
--- src/pages/mainPage.tsx
+++ src/pages/mainPage.tsx
@@ -1,30 +1,25 @@
-
-import Describe1 from '../component/describe1';
-import Footer from '../component/footer';
-import LogoHero from '../component/utils/logoHero';
-
+import Describe1 from "../component/describe1";
+import Footer from "../component/footer";
+import LogoHero from "../component/utils/logoHero";
 
 export default function MainPage() {
   return (
     <>
-
       <LogoHero
         text1="The easiest way to control your Data"
         subText1="yourd에 관한 요약 설명내용"
       />
       <Describe1
-        text1={'Ignite Data Ownership'}
-        subText1={'Take Control of Your Data Ownership'}
+        text1={"Ignite Data Ownership"}
+        subText1={"Take Control of Your Data Ownership"}
         text2={
-          'We lead the revolutionary change in personal data ownership for you'
+          "We lead the revolutionary change in personal data ownership for you"
         }
         text3={
-          'Shape the future of data ownership and join us in a more secure and person-centric digital world'
+          "Shape the future of data ownership and join us in a more secure and person-centric digital world"
         }
       />
       <Footer />
-
-  
     </>
   );
 }
