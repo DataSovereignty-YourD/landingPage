dc25a2ea67634695b7262361ebd1917d3bdbb86f
4uphwang <4uphwang@gmail.com>
2023-09-30 00:47:55 +0900

    ccpa

--- src/components/yourdStack.jsx
+++ src/components/yourdStack.jsx
@@ -8,7 +8,7 @@ export default function YourdStack() {
   return (
     <div className="w-5/6 mx-auto h-screen flex flex-col justify-center items-center">
       <div className="font-sf-pro-text font-bold text-center w-full py-20 text-xl sm:mx-0 sm:text-3xl relative"> USE YourD, the easiest way to build web3.0 project</div>
-      <img src={YourDStack} alt="YourDStack" className=" object-contain w-full lg:w-4/5 bg-white relative mx-auto"/>
+      <img src={YourDStack} alt="YourDStack" className=" object-contain w-full lg:w-3/5 bg-white relative mx-auto"/>
     </div>
   );
 }
