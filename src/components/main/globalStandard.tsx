cddb89383907a815a0f504988f1e4505dabddc31
4uphwang <4uphwang@gmail.com>
2023-10-02 23:18:12 +0900

    responsive

--- src/components/main/globalStandard.tsx
+++ src/components/main/globalStandard.tsx
@@ -39,19 +39,17 @@ export default function GlobalStandard() {
   ];
 
   return (
-    <div className="w-screen justify-center items-center h-[60vh] flex flex-col relative font-sf-pro-text text-center bg-blue-50 z-30">
+    <div className="px-5 sm:px-10 md:w-full md:px-[12.5%] justify-center items-center min-h-[60vh] flex flex-col relative font-sf-pro-text text-center py-5 bg-blue-50 z-30">
       <>
-        <div className="uppercase text-lg sm:text-4xl text-center font-bold mb-10 px-5 w-full">
+        <div className="uppercase text-lg md:text-2xl lg:text-4xl text-center font-bold mb-5 md:mb-10  px-5 w-full">
           GLOBAL RECOGNITION & STANDARDIZATION
         </div>
-        <div className="font-medium text-sm px-4 sm:text-2xl max-w-[1000px]">
-          With global standards, W3C compliance, award-winning blockchain
-          hackathons, and backing from Tezos, we are committed to enabling
-          effective data ownership.
+        <div className="font-normal md:font-medium text-sm px-4 md:text-2xl max-w-[1000px]">
+        Adhering to the global standard of W3C,, backing from Tezos, and recognized in blockchain hackathon, we are committed to enabling effective data ownership.
         </div>
       </>
-      <div className="flex flex-wrap gap-3 max-w-[1000px] items-center justify-center">
-        {Logos.map(logo=> <img key={logo.title} src={logo.img} alt={logo.title}  className="h-12 sm:h-16 mt-14 backdrop-blur-sm object-contain"/>)}
+      <div className="flex flex-wrap gap-3 w-full max-w-[1000px] items-center justify-center">
+        {Logos.map(logo=> <img key={logo.title} src={logo.img} alt={logo.title}  className="h-6 sm:h-8 md:h-10 mt-4 md:mt-14 backdrop-blur-sm object-contain"/>)}
       </div>
     </div>
   );
