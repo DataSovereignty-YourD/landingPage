f0d7ccf5c4dcec2536c4a1006cabed772bb5c1e4
yeomjaeseung <struckpang@gmail.com>
2023-10-06 18:35:19 +0900

    globalstandard apple

--- src/components/common/topBar.jsx
+++ src/components/common/topBar.jsx
@@ -8,24 +8,20 @@ export default function TopBar() {
   const [lastScrollY, setLastScrollY] = useState(0);
 
   return (
-    <div className="bg-white h-16  w-full flex justify-between px-4 sm:px-[5%] fixed z-50">
+    <div className="bg-white h-16 w-full flex justify-between items-center px-4 sm:px-[5%] fixed z-50 gap-x-4">
       <Link to="/">
-        <img
-          src={logo}
-          alt="logo"
-          className=" flex items-center w-12 sm:w-16"
-        />
+        <img src={logo} alt="logo" className="flex items-center w-12 sm:w-16" />
       </Link>
-      <div className=""></div>
-      <div className=" flex flex-row  sm:gap-2 items-center justify-center">
-        <div className=" w-fit sm:min-w-32">
+
+      <div className="flex flex-row sm:gap-2 items-center justify-center">
+        <div className="w-fit sm:min-w-32">
           <Link to="/contact">
             <RoundedButton />
           </Link>
         </div>
-        {/* <div className=" w-10 h-10 flex items-center  ">
+        <div className="block sm:hidden items-center">
           <SideNav />
-        </div> */}
+        </div>
       </div>
     </div>
   );
