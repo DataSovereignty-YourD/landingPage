c789a569256d1b823843f4811de904f171f14c4a
yeomjaeseung <struckpang@gmail.com>
2024-01-09 16:51:58 +0900

    apple

--- src/components/about/yourdTeam.jsx
+++ src/components/about/yourdTeam.jsx
@@ -1,25 +1,62 @@
+import profileImage from '../../assets/img/profile.png';
+import { FaLinkedinIn } from 'react-icons/fa';
+import { FaXTwitter } from 'react-icons/fa6';
+import Back from '../../assets/img/back.png';
+import { Link } from 'react-router-dom';
+
+
+const profiles = [
+  { name: 'J.G', position: 'CEO / Co-Founder', sns1: 'https://www.linkedin.com/in/j-g-y-a7b429244/', sns2: 'https://twitter.com/SSi3_0' },
+  { name: 'Cha', position: 'CTO / Co-Founder', sns1: 'https://www.linkedin.com/in/younghoon-cha-1173a5248/', sns2: 'https://twitter.com/yhc125' },
+  { name: 'Hwang', position: 'COO / Co-Founder', sns1: 'https://www.linkedin.com/in/ji-hwang-kim-10a539243/', sns2: 'https://twitter.com/unchain_hwang' },
+  { name: 'Miix', position: 'Software Engineer', sns1: 'https://www.linkedin.com/in/miix-y-723b44271/', sns2: 'https://twitter.com/yeomjaeseu98392' },
+  { name: 'Jake', position: 'Software Engineer', sns1: '', sns2: 'https://twitter.com/bagseon52525961' },
+  { name: 'J.G', position: 'Product Manager', sns1: 'https://www.linkedin.com/in/hyunchae-jo/', sns2: 'https://twitter.com/trip4e_J' },
+  { name: 'J.G', position: 'Designer', sns1: 'https://www.linkedin.com/in/%EC%98%88%EC%B0%AC-%EB%B0%95-36211b279/', sns2: 'https://twitter.com/Farrell_YourD' },
+];
+
 export default function YourdTeam() {
   return (
-    <div className="flex items-center justify-center gap-32 flex-col h-[800px]">
-      <div className="text-5xl font-bold ">OUR TEAM</div>
-      <div className="mb-32">
-        블록체인 신원 인증 분야에서 혁신적인 작업을 진행 중입니다. 2021년부터
-        시작하여 다양한 블록체인 기반 프로젝트를 탐구하고 연구하였습니다. 현재
-        블록체인을 활용한 신원 인증, 보안 솔루션 및 활용 서비스를 제공하고
-        있습니다. 이를 통해 개인정보의 안전성을 높이고, 더욱 투명하고 안전한
-        디지털 환경을 구축하는 것이 우리 팀의 주요 목표입니다.
+    <div className="flex-col min-h-[800px] px-10 flex justify-start">
+      <div className="mt-28 text-4xl font-pre-bold gradientText">
+        Meet the Innovators
       </div>
-      <div className=" border border-b-4 border-black w-full"></div>
-      <div className="flex flex-row gap-48 transform -translate-y-36">
-        <div className="rounded-full border w-[30px] h-[30px] bg-blue-700"></div>
-
-        <div className="rounded-full border w-[30px] h-[30px] bg-blue-700"></div>
-        <div className="rounded-full border w-[30px] h-[30px] bg-blue-700"></div>
-        <div className="rounded-full border w-[30px] h-[30px] bg-blue-700"></div>
-        <div className="rounded-full border w-[30px] h-[30px] bg-blue-700"></div>
-        <div className="rounded-full border w-[30px] h-[30px] bg-blue-700"></div>
-        <div className="rounded-full border w-[30px] h-[30px] bg-blue-700"></div>
+      <div className="mt-4 text-xl font-pre-regular">
+        Behind the Scenes of Web 3.0 Revolution
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-3 gap-20 mt-32">
+        {profiles.map((profile, index) => (
+          <ProfileCard key={index} profile={profile} />
+        ))}
       </div>
     </div>
   );
 }
+const ProfileCard = ({ profile }) => {
+  return (
+    <div className="w-[280px] mx-auto bg-white rounded-lg shadow-md overflow-hidden">
+      <div className="relative text-white py-4 text-center">
+        <img src={Back} alt="Background" className="absolute inset-0 w-full h-full object-cover" />
+        <div className="relative z-10 p">
+          <img src={profileImage} alt="Profile" className="w-24 h-24 rounded-full mx-auto border-4 border-white"/>
+        </div>
+      </div>
+
+      <div className="flex flex-col items-center pb-10">
+        <div className="mt-5">
+          <p className="text-xl font-semibold">{profile.name}</p>
+          <p className="text-sm text-gray-600">{profile.position}</p>
+        </div>
+        <div className="flex mt-4 space-x-3">
+          <a href={profile.sns1} target="_blank" rel="noopener noreferrer">
+            <FaLinkedinIn size={24} className="hover:text-yellow-400 cursor-pointer" />
+          </a>
+          <a href={profile.sns2} target="_blank" rel="noopener noreferrer">
+            <FaXTwitter size={24} className="hover:text-yellow-400 cursor-pointer" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
--- src/components/solutions/payment/paymentProcess.jsx
+++ src/components/solutions/payment/paymentProcess.jsx
@@ -1,49 +1,58 @@
-import process1 from '../../../assets/img/process1.png';
-import process2 from '../../../assets/img/process2.png';
-import process3 from '../../../assets/img/process3.png';
-import process4 from '../../../assets/img/process4.png';
+import process1 from "../../../assets/img/process1.png";
+import process2 from "../../../assets/img/process2.png";
+import process3 from "../../../assets/img/process3.png";
+import process4 from "../../../assets/img/process4.png";
 export default function PaymentProcess() {
   const process = [
     {
       id: 1,
-      title: 'Select YourD Pay',
-      subtitle: 'Select YourD Pay on the e-commerce platform',
+      title: "Select YourD Pay",
+      subtitle: "Select YourD Pay on the e-commerce platform",
       imgsrc: process1,
     },
     {
       id: 2,
-      title: 'Scan QR',
-      subtitle: 'Scan the QR code with payment information',
+      title: "Scan QR",
+      subtitle: "Scan the QR code with payment information",
       imgsrc: process2,
     },
     {
       id: 3,
-      title: 'Card Selection',
-      subtitle: 'Please select the card for payment',
+      title: "Card Selection",
+      subtitle: "Please select the card for payment",
       imgsrc: process3,
     },
     {
       id: 4,
-      title: 'Settlement',
-      subtitle: 'Payment approved and transaction completed',
+      title: "Settlement",
+      subtitle: "Payment approved and transaction completed",
       imgsrc: process4,
     },
   ];
   return (
-    <div className="flex-col flex justify-center items-center px-10">
-      <div className= 'mt-10 text-4xl'>Your<span className='text-[#fccc00]'>D</span>&nbsp;<span className='font-pre-bold bg-clip-text bg-gradient-to-r from-gradient1 to-gradient2 text-transparent w-fit  x'>Payment Process</span></div>
-      <div className="flex items-center justify-center mt-10 mb-20">
-        {process.map((process) => (
-          <div className='sm:col-span-2  flex-col flex items-center justify-center w-full'>
-            <div className="w-[60px] bg-yellow-200 h-[60px] rounded-full flex items-center justify-center">
-              <div className="text-2xl font-bold"> {process.id}</div>
-            </div>
-            <div className="font-bold text-3xl mt-6">{process.title}</div>
-            <div className='text-2xl font-semibold text-center mt-8'>{process.subtitle}</div>
-             <div className='mt-10'><img src={process.imgsrc} className='w-[260px]' /></div> 
-          </div>
-        ))}
+    <div className="flex-col items-center justify-center px-10">
+    <div className="mt-10 sm:mt-16 sm:text-5xl text-4xl font-bold ">
+      <div className="flex items-center justify-center">Your<span className="text-[#fccc00]">D</span>&nbsp; <br /></div>
+      <div className="flex items-center justify-center font-pre-bold bg-clip-text bg-gradient-to-r from-gradient1 to-gradient2 text-transparent ">
+        Payment Process
       </div>
     </div>
+    <div className="mt-10 sm:mb-20 sm:flex-row flex-col flex sm:mt-16">
+      {process.map((process) => (
+        <div key={process.id} className="flex flex-col sm:flex-col items-center justify-center w-full pb-12">
+          <div className="w-[30px] sm:w-[60px] sm:h-[60px] bg-yellow-300 h-[30px] rounded-full flex items-center justify-center">
+            <div className="text-2xl font-bold"> {process.id}</div>
+          </div>
+          <div className="font-bold text-3xl mt-6">{process.title}</div>
+          <div className="text-2xl font-semibold text-center mt-2 sm:mt-4 text-gray-400">
+            {process.subtitle}
+          </div>
+          <div className="mt-10">
+            <img src={process.imgsrc} className="w-[260px]" alt={`Process ${process.id}`} />
+          </div>
+        </div>
+      ))}
+    </div>
+  </div>
   );
 }
