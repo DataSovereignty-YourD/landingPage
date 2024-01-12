d68fa24e01ad5905c18ef2cfaa0b9b55b0ae4cf4
yeomjaeseung <struckpang@gmail.com>
2024-01-12 20:29:21 +0900

    apple

--- src/components/about/yourdMissionDescribe.jsx
+++ src/components/about/yourdMissionDescribe.jsx
@@ -2,15 +2,15 @@ export default function YourdMissionDescribe() {
   return (
     <div className=" h-80 w-full   flex items-center justify-center ">
       <div className=" flex flex-col gap-4 items-center justify-center relative">
-        <div className="  text-4xl font-semibold  ">
+        <div className=" text-2xl sm:text-4xl font-semibold  ">
           Your<span className="text-[#fccc00]">D</span> is leading the way for
           the true web3.0
         </div>
-        <div className="  text-xl">
-          Your<span className="text-[#fccc00]">D</span>
-          is a company that builds Web 3.0 infrastructure through authentication
-          and payment. Our goal is to make your first steps into the Web 3.0
-          world as easy as possible.
+        <div className="sm:text-2xl">
+          Your<span className="text-[#fccc00]">D</span>&nbsp;
+           is a company that builds Web 3.0 infrastructure through authentication
+          and payment.<br/><div className="mt-2 sm:text-center">Our goal is to make your first steps into the Web 3.0
+          world as easy as possible.</div> 
         </div>
       </div>
     </div>
