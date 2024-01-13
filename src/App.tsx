56f7ab42794cd052b5fc198cbd903c4778749804
yeomjaeseung <struckpang@gmail.com>
2024-01-13 23:57:07 +0900

    apple

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import { Routes, Route } from "react-router-dom";
-import React from "react";
-import MainPage from "./pages/mainPage";
-import TopBar from "./components/common/topBar";
-import AboutPage from "./pages/aboutPage";
-import ContactUs from "./components/common/contactUs";
-import Blog from "./components/blog/blog";
-import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
-import YourDLoginPage from "./pages/product/yourdLoginPage";
-import YourDPassPage from "./pages/product/yourdPassPage";
-import YourDAnalyticsPage from "./pages/product/yourdAnalayticsPage";
-import YourDInfraPage from "./pages/product/yourdInfraPage";
-import YourDPaymentPage from "./pages/product/yourdPaymentPage";
-import { preloadedImageState, imageUrlState } from './state/imageState';
-import { useRecoilValue, useSetRecoilState } from "recoil";
+import { Routes, Route } from 'react-router-dom';
+import React from 'react';
+import MainPage from './pages/mainPage';
+import TopBar from './components/common/topBar';
+import AboutPage from './pages/aboutPage';
+import ContactUs from './components/common/contactUs';
+import Blog from './components/blog/blog';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import YourDLoginPage from './pages/product/yourdLoginPage';
+import YourDPassPage from './pages/product/yourdPassPage';
+import YourDAnalyticsPage from './pages/product/yourdAnalayticsPage';
+import YourDInfraPage from './pages/product/yourdInfraPage';
+import YourDPaymentPage from './pages/product/yourdPaymentPage';
+
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 
 function App() {
   const imageUrls = [
@@ -23,23 +23,17 @@ function App() {
     './assets/img/paymentMain.png',
     './assets/img/yourdpass-main.png',
   ];
+  useEffect(() => {
+    imageUrls.forEach((url) => {
+      const img = new Image();
+      img.src = url;
+    });
+  }, []);
   const location = useLocation();
-    // 이미지 URL 설정
-    const setImageUrl = useSetRecoilState(imageUrlState);
-    useEffect(() => {
-      setImageUrl('path/to/your/image.jpg'); // 이미지 URL 설정
-    }, [setImageUrl]);
-  
-    // 이미지 미리 로드
-    const preloadedImageUrl = useRecoilValue(preloadedImageState);
-    useEffect(() => {
-      imageUrls.forEach(url => {
-        const img = new Image();
-        img.src = url;
-        // 필요한 경우 여기서 로드 완료나 오류 처리를 할 수 있습니다.
-      });
-    }, []);
-    
+  // 이미지 URL 설정
+
+  // 이미지 미리 로드
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [location.pathname]);
