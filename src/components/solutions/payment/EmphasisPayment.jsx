21f16770b7c12ca8917336bade286dbb976efad5
parkerynch <110054754+parkerynch@users.noreply.github.com>
2024-01-15 23:11:45 +0900

    modify part of emphasispayment and change image of authdiagram

--- src/components/solutions/payment/EmphasisPayment.jsx
+++ src/components/solutions/payment/EmphasisPayment.jsx
@@ -1,23 +1,23 @@
-import React from 'react';
-import LocationPayment from '../../../assets/img/locationPayment.png';
-import ModulePayment from '../../../assets/img/paymentModule.png';
-import Location2 from '../../../assets/img/location2.png';
-import Example from './example';
+import React from "react";
+import LocationPayment from "../../../assets/img/locationPayment.png";
+import ModulePayment from "../../../assets/img/paymentModule.png";
+import Location2 from "../../../assets/img/location2.png";
+import Example from "./example";
 const EmphasisSections = [
   {
-    title: 'Without location restrictions',
+    title: "Without location restrictions",
     content: [
-      'We provide a universal payment solution that can be used in both the real world and virtual space.',
-      'Pay easily from anywhere.',
+      "We provide a universal payment solution that can be used in both the real world and virtual space.",
+      "Pay easily from anywhere.",
       "Design complex crypto payment systems easily so users don't need to know anything technical.",
     ],
     image: LocationPayment,
   },
   {
-    title: 'Payment module provided',
+    title: "Payment module provided",
     content: [
-      'Easily integrate into your business with an efficient and powerful payment module.',
-      'Experience continuous innovation by responding to new payment trends through user-oriented design and ease of upgrade.',
+      "Easily integrate into your business with an efficient and powerful payment module.",
+      "Experience continuous innovation by responding to new payment trends through user-oriented design and ease of upgrade.",
     ],
     image: ModulePayment,
   },
@@ -58,18 +58,20 @@ export default function EmphasisPayment() {
           </div>
         </div>
       ))} */}
-      <div className="flex flex-col sm:flex-row gap-10 sm:px-12 md:px-16 lg:px-12 w-full">
-        <div className=" bg-white rounded-xl px-4 my-16 py-4 drop-shadow-lg order-1 sm:order-none">
-          <div className="text-xl sm:text-2xl md:text-4xl lg:text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 to-orange-400">
-            Without location restrictions
+      <div className="flex flex-col sm:flex-row  sm:px-12 md:px-16 lg:px-12 w-full ">
+        <div className=" rounded-xl px-4 my-16 py-4 drop-shadow-lg order-1 sm:order-none items-center justify-center flex flex-col ">
+          <div className="text-xl sm:text-2xl md:text-4xl lg:text-5xl font-pre-regular text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 to-orange-400">
+            Anywhere in the world!
           </div>
-          <div className="mt-14 sm:text-lg md:text-2xl  ">
-            We provide a universal payment solution that can be used in both the
-            real world and virtual space.
-            <div className="mt-4"> Pay easily from anywhere.</div>
+          <div className="mt-10 sm:text-lg md:text-2xl">
+            <div>
+              YourD Pay offers an innovative payment solution accessible in both
+              the physical and virtual realms.
+            </div>
+
             <div className="mt-4">
-              Design complex crypto payment systems easily so users don't need
-              to know anything technical.
+              Simplify and enhance your daily life with easy payments anytime,
+              anywhere.
             </div>
           </div>
         </div>
@@ -84,14 +86,14 @@ export default function EmphasisPayment() {
           className="w-full sm:w-2/5 lg:w-2/5 object-contain"
         />
         <div className="flex sm:gap-28">
-          <div className=" bg-white rounded-xl px-4 py-4 my-16 drop-shadow-lg">
-            <div className="text-xl sm:text-2xl md:text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 to-orange-400">
+          <div className=" rounded-xl px-4 py-4 my-16 drop-shadow-lg items-center justify-center flex flex-col">
+            <div className="text-xl sm:text-2xl md:text-4xl lg:text-5xl font-pre-regular text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 to-orange-400 pb-1">
               Payment module provided
             </div>
-            <div className="mt-14 sm:text-lg md:text-2xl">
+            <div className="mt-10 sm:text-lg md:text-2xl">
               Easily integrate into your business with an efficient and powerful
               payment module.
-              <div className="mt-10">
+              <div className="mt-4">
                 Experience continuous innovation by responding to new payment
                 trends through user-oriented design and ease of upgrade.
               </div>
