403931ccde84dad58ab82d6262ed8f3eec2e6fa6
parkerynch <110054754+parkerynch@users.noreply.github.com>
2024-08-14 02:21:51 +0900

    logo add in awards page

--- src/components/about/yourdAwards.jsx
+++ src/components/about/yourdAwards.jsx
@@ -1,18 +1,24 @@
-import { GoPerson } from 'react-icons/go';
-import Tron from '../../assets/img/tronLogo.png';
-import Eth from '../../assets/img/ehtereumLogo.png';
-import Polkadot from '../../assets/img/polkadotLogo.png';
-import Evmos from '../../assets/img/evmosLogo.png';
-import Klaytn from '../../assets/img/klaytnLogo.png';
-import Tzapc from '../../assets/img/tzapc.png';
-import googlecloud from '../../assets/img/googlecloud.png';
-import Ebsi from '../../assets/img/ebsieuro.png';
-import W3C from '../../assets/img/w3cimg.png';
-import Tezos from '../../assets/img/tezosLogo.png';
+import { GoPerson } from "react-icons/go";
+import Tron from "../../assets/img/tronLogo.png";
+import Eth from "../../assets/img/ehtereumLogo.png";
+import Polkadot from "../../assets/img/polkadotLogo.png";
+import Evmos from "../../assets/img/evmosLogo.png";
+import Klaytn from "../../assets/img/klaytnLogo.png";
+import Tzapc from "../../assets/img/tzapc.png";
+import googlecloud from "../../assets/img/googlecloud.png";
+import Ebsi from "../../assets/img/ebsieuro.png";
+import W3C from "../../assets/img/w3cimg.png";
+import Tezos from "../../assets/img/tezosLogo.png";
+import Kaia from "../../assets/img/kaiaLogo.png";
+import Coineasy from "../../assets/img/coineasyLogo.png";
+import Polygon from "../../assets/img/polygonLogo.png";
+import NearKoreaDao from "../../assets/img/nearkoreadaoLogo.png";
 export default function YourdAwards() {
   return (
     <div className="min-h-[800px] relative mt-32">
-      <div className='flex justify-center items-center text-xl sm:text-4xl font-pre-bold mt-20'>Awards</div>
+      <div className="flex justify-center items-center text-xl sm:text-4xl font-pre-bold mt-20">
+        Awards
+      </div>
       <div className="flex justify-center items-center gap-12 sm:gap-20 mt-10">
         <img src={Tron} className="w-[70px] sm:w-[250px]" />
         <img src={Polkadot} className="w-[70px] sm:w-[250px]" />
@@ -22,16 +28,30 @@ export default function YourdAwards() {
         <img src={Evmos} className="w-[70px] sm:w-[250px]" />
         <img src={Klaytn} className="w-[70px] sm:w-[250px]" />
       </div>
-      <div className='mt-20 text-xl sm:text-4xl font-pre-bold flex justify-center items-center'>Standards Achievement</div>
-      <div className="flex justify-center items-center gap-20 mt-10">
-        <img src={W3C} className='w-[50px] sm:w-[170px]'/>
-        <img src={Ebsi} className='w-[120px] sm:w-[300px]'/>
+      <div className="mt-20 text-xl sm:text-4xl font-pre-bold flex justify-center items-center">
+        Standards Achievement
       </div>
-      <div className='mt-12 text-xl sm:text-4xl font-pre-bold flex justify-center items-center'>Partners</div>
+      <div className="flex justify-center items-center gap-20 mt-10">
+        <img src={W3C} className="w-[50px] sm:w-[170px]" />
+        <img src={Ebsi} className="w-[120px] sm:w-[300px]" />
+      </div>
+      <div className="mt-12 text-xl sm:text-4xl font-pre-bold flex justify-center items-center">
+        Partners
+      </div>
       <div className="flex justify-center items-center gap-8 sm:gap-20 mt-2">
-        <img src={Tezos} className='w-[100px] sm:w-[250px]'/>
-        <img src={Tzapc} className='w-[100px] sm:w-[250px]'/>
-        <img src={googlecloud} className='w-[100px] sm:w-[250px]'/>
+        <img src={Tezos} className="w-[100px] sm:w-[250px]" />
+        <img src={Tzapc} className="w-[100px] sm:w-[250px]" />
+        <img src={googlecloud} className="w-[100px] sm:w-[250px]" />
+      </div>
+      <div className="flex justify-center items-center gap-8 sm:gap-20 mt-2">
+        <img src={Kaia} className="w-[100px] sm:w-[250px]" />
+        <img src={Klaytn} className="w-[100px] sm:w-[250px]" />
+        <img src={Polygon} className="w-[100px] sm:w-[250px]" />
+      </div>
+      <div className="flex justify-center items-center gap-8 sm:gap-20 mt-2">
+        <img src={NearKoreaDao} className="w-[100px] sm:w-[250px]" />
+        <img src={Coineasy} className="w-[100px] sm:w-[250px]" />
+        <img src={""} className="w-[100px] sm:w-[250px]" />
       </div>
     </div>
   );
